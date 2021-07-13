b386bc9c58f76044697b1a7357ee7d51c35ff31c
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2021-07-13 19:48:53 +1000

    Added bookmarklet

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,34 +1,30 @@
 (async function () {
-  function sleep(ms: number) {
+  function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  function VideoDuration(duration: number, minusBy: number = 300): number {
+  function VideoDuration(duration, minusBy = 300) {
     return duration * 1000 - minusBy;
   }
 
   async function RefreshPage() {
-    const closeBtn = document.querySelector(".close") as HTMLImageElement;
-    const currentVideo = document.querySelector(
-      "span.event-delegate-mask"
-    ) as HTMLSpanElement;
+    const closeBtn = document.querySelector(".close");
+    const currentVideo = document.querySelector("span.event-delegate-mask");
 
     closeBtn?.click();
     await sleep(3000);
     currentVideo?.click();
     await sleep(1000);
-    (document.querySelector(".arrow-right") as HTMLImageElement)?.click();
+    document.querySelector(".arrow-right")?.click();
     await sleep(1000);
   }
   if (document.querySelector(".lazyload-wrapper")) {
-    (
-      document.querySelector(
-        ".lazyload-wrapper span.event-delegate-mask"
-      ) as HTMLSpanElement
-    )?.click();
+    document
+      .querySelector(".lazyload-wrapper span.event-delegate-mask")
+      ?.click();
   }
   await sleep(1000);
-  let downBtn = document.querySelector(".arrow-right") as HTMLImageElement;
+  let downBtn = document.querySelector(".arrow-right");
   let video = document.querySelector("video");
   while (true) {
     await sleep(VideoDuration(video.duration, 740));
