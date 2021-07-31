43ef258e3be551ad69b8ad3f7bd5110c8b07e3e2
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2021-07-31 22:30:31 +1000

    Updated the way it reloads the tiktoks

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,6 +1,7 @@
 (async function () {
-  if(!window.location.host.toLowerCase().includes("tiktok.com")) return console.log("Auto-TikTok-Scroller Error: Not on TikTok website.");
-  else console.log("Auto-TikTok-Scroller Log: Starting bookmarklet...")
+  if (!window.location.host.toLowerCase().includes("tiktok.com"))
+    return console.log("Auto-TikTok-Scroller Error: Not on TikTok website.");
+  else console.log("Auto-TikTok-Scroller Log: Starting bookmarklet...");
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
@@ -8,19 +9,6 @@
   function VideoDuration(duration, minusBy = 300) {
     return duration * 1000 - minusBy;
   }
-
-  async function LoadVideos() {
-    console.log("Auto-TikTok-Scroller Log: Loading Videos...");
-    for (let i = 0; i < 8; i++) {
-      let videos = Array.from(document.querySelectorAll(".lazyload-wrapper"));
-      videos[videos.length - 1].scrollIntoView({
-        block: "end",
-        inline: "nearest",
-      });
-      await sleep(3000);
-    }
-  }
-  await LoadVideos();
   if (document.querySelector(".lazyload-wrapper")) {
     document
       .querySelector(".lazyload-wrapper span.event-delegate-mask")
@@ -31,10 +19,10 @@
   let video = document.querySelector("video");
   console.log("Auto-TikTok-Scroller Log: Starting scrolling...");
   while (true) {
-    await sleep(VideoDuration(video.duration, 740));
+    await sleep(VideoDuration(video.duration, 1310));
     if (document.querySelector(".arrow-right")) downBtn.click();
-    else return;
-    await sleep(1000);
+    else return reload();
+    await sleep(1600);
     video = document.querySelector("video");
   }
 })();
